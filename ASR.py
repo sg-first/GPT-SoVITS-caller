d89a65ca43a8284144756c12b214c39f531b9e02
Knightmaremoon <3573729289@qq.com>
2024-06-05 00:41:20 +0800

    封装成函数

--- ASR.py
+++ ASR.py
@@ -1,16 +1,20 @@
 import webui_duplicate
 
-if __name__ == "__main__":
-    # Example of how to call the function with parameters
+def asr(asr_inp_dir,asr_opt_dir ):
     result = webui_duplicate.open_asr(
-        asr_inp_dir = 'E:\\BaiduNetdiskDownload\\GPT-SoVITS-beta0306fix2\\output\\slicer_opt',
-        asr_opt_dir = 'output/asr_opt',
-        asr_model = 'Faster Whisper (多语种)',
-        asr_model_size = 'small',
-        asr_lang = 'auto'
+        asr_inp_dir=asr_inp_dir,
+        asr_opt_dir=asr_opt_dir,
+        asr_model='Faster Whisper (多语种)',
+        asr_model_size='small',
+        asr_lang='auto'
     )
 
     print(str(result))
 
     result = webui_duplicate.close_asr()
     print(result)
+
+if __name__ == "__main__":
+    asr_inp_dir = 'E:\\BaiduNetdiskDownload\\GPT-SoVITS-beta0306fix2\\output\\slicer_opt'
+    asr_opt_dir = 'output/asr_opt'
+    asr(asr_inp_dir, asr_opt_dir)
