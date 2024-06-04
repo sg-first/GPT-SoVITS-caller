15aa962b04f8cf03babb3d28f369759901440760
Knightmaremoon <3573729289@qq.com>
2024-06-05 00:38:23 +0800

    封装成函数

--- denoise.py
+++ denoise.py
@@ -1,13 +1,18 @@
 import webui_duplicate
 
-if __name__ == "__main__":
-    # Example of how to call the function with parameters
+def denoise(denoise_inp_dir, denoise_opt_dir):
+
     result = webui_duplicate.open_denoise(
-        denoise_inp_dir = 'E:\\BaiduNetdiskDownload\\GPT-SoVITS-beta0306fix2\\output\\slicer_opt',
-        denoise_opt_dir = 'E:\\BaiduNetdiskDownload\\GPT-SoVITS-beta0306fix2\\output\\denoise_opt'
+        denoise_inp_dir=denoise_inp_dir,
+        denoise_opt_dir=denoise_opt_dir
     )
 
     print(str(result))
 
     result = webui_duplicate.close_denoise()
     print(result)
+
+if __name__ == "__main__":
+    denoise_inp_dir = 'E:\\BaiduNetdiskDownload\\GPT-SoVITS-beta0306fix2\\output\\slicer_opt'
+    denoise_opt_dir = 'E:\\BaiduNetdiskDownload\\GPT-SoVITS-beta0306fix2\\output\\denoise_opt'
+    denoise(denoise_inp_dir, denoise_opt_dir)
