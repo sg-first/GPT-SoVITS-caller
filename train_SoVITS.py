ebbaea311324840e7cb08946cffed79bf0b4953d
Knightmaremoon <3573729289@qq.com>
2024-06-04 22:53:58 +0800

    添加结束进程逻辑+封装成便于调用的函数

--- train_SoVITS.py
+++ train_SoVITS.py
@@ -1,18 +1,26 @@
 import webui_duplicate
 
-if __name__ == "__main__":
+def train_so_vits(exp_name, gpu_numbers1Ba):
     # Example of how to call the function with parameters
     result = webui_duplicate.open1Ba(
-        batch_size=2,
-        total_epoch=8,
-        exp_name='test',
-        text_low_lr_rate=0.4,
-        if_save_latest=True,
-        if_save_every_weights=True,
-        save_every_epoch=8,
-        gpu_numbers1Ba='0',
-        pretrained_s2G="GPT_SoVITS/pretrained_models/s2G488k.pth",
-        pretrained_s2D="GPT_SoVITS/pretrained_models/s2D488k.pth"
+        batch_size = 2,
+        total_epoch = 8,
+        exp_name = exp_name,
+        text_low_lr_rate = 0.4,
+        if_save_latest = True,
+        if_save_every_weights = True,
+        save_every_epoch = 8,
+        gpu_numbers1Ba = gpu_numbers1Ba,
+        pretrained_s2G = "GPT_SoVITS/pretrained_models/s2G488k.pth",
+        pretrained_s2D = "GPT_SoVITS/pretrained_models/s2D488k.pth"
     )
-
     print(str(result))
+
+    result = webui_duplicate.close1Ba()
+    print(result)
+
+if __name__ == "__main__":
+
+    exp_name = 'test'
+    gpu_numbers1Ba = '0'
+    train_so_vits(exp_name, gpu_numbers1Ba)
